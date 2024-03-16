f24d191c90473a1770571e96660945267226548c
MatthewMattei <96449493+MatthewMattei@users.noreply.github.com>
2024-03-15 17:34:45 -0700

    name fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ By: Matthew Mattei
 Date: 3/14/24
 Credits: ChatGPT was used for planning and writing elements of the code in this file
 and the unigram_freq.csv dataset used in this file was uploaded and made
-publically availabe by Rachel Tatman on Kaggle
+publically availabe by Rachael Tatman on Kaggle
 """
 
 # Imports
